b7b6db1d3282df35c99587cd970b2da3e6236fca
Anderson Orozco <anderson93428@gmail.com>
2026-03-17 22:09:21 -0600

    Fix functions folder

--- Functions/download.js
+++ Functions/download.js
@@ -1,44 +1,24 @@
 const fs = require('fs');
 const play = require('play-dl');
-const { exec } = require('child_process');
 
-// 🔊 AUDIO
+// 🎵 AUDIO
 async function downloadAudio(url) {
-
   const file = "audio.mp3";
 
   const stream = await play.stream(url, { quality: 2 });
-
   const writeStream = fs.createWriteStream(file);
 
   return new Promise((resolve, reject) => {
-
     stream.stream.pipe(writeStream);
 
-    writeStream.on('finish', () => {
-      resolve(file);
-    });
-
+    writeStream.on('finish', () => resolve(file));
     writeStream.on('error', reject);
-
   });
-
 }
 
-// 🎬 VIDEO
+// 🎬 VIDEO (básico por ahora)
 async function downloadVideo(url) {
-
-  const file = "video.mp4";
-
-  return new Promise((resolve, reject) => {
-
-    exec(`ffmpeg -i "${url}" -c copy ${file}`, (error) => {
-      if (error) return reject(error);
-      resolve(file);
-    });
-
-  });
-
+  throw new Error("Video no soportado aún en versión Railway");
 }
 
 module.exports = {
